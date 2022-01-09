aeaaf9e0e9f0ba454f72f5e326b6a9ac248c35e6
Tom <tom@grumpy.engineering>
2022-01-09 17:06:15 +0000

    Making it neater

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,6 @@
 
 #define RX_PIN PA11
 #define TX_PIN PA9
-#define EN_PIN PA10
-
-#define TXPIN   PA9
 
 OBD9141 obd;
 
@@ -38,129 +35,80 @@ void dlcInit(void)
     delay(300);
 }
 
-void dlcCommand(byte cmd, byte num, byte loc, byte len) 
+int8_t dlcRead(uint8_t address, uint8_t length, uint8_t * rxData) 
 {
-    byte crc = (0xFF - (cmd + num + loc + len - 0x01)); // checksum FF - (cmd + num + loc + len - 0x01)
-
-    unsigned long timeOut = millis() + 250; // timeout @ 250 ms
-
-    Serial1.write(cmd);  // header/cmd read memory ??
-    Serial1.write(num);  // num of bytes to send
-    Serial1.write(loc);  // address
-    Serial1.write(len);  // num of bytes to read
-    Serial1.write(crc);  // checksum
-
-    int i = 0;
+    uint8_t crc;
+    uint32_t timeout = millis() + 250; // timeout @ 250 ms
+    int8_t byteCount;
     uint8_t data;
 
+    // Calculate CRC
+    crc = 0x20;
+    crc += 0x05;
+    crc += address;
+    crc += length;
+    crc = crc - 0x01;
+    crc = 0xff - crc;
+
+    byteCount = -5;
+
+    Serial1.write(0x20);     // header/cmd read memory
+    Serial1.write(0x05);        // num of bytes to send
+    Serial1.write(address);  // address
+    Serial1.write(length);  // num of bytes to read
+    Serial1.write(crc);  // checksum
+
     Serial.print("Data: ");
-    while (millis() < timeOut) 
+    while (byteCount < (length + 3) && millis() < timeout) 
     {
         if (Serial1.available()) 
         {
+            byteCount++;
             data = Serial1.read();
             PrintHex8(data);
-            i++;
+            if(byteCount >= 0)
+                rxData[byteCount] = data;
         }
     }
     Serial.println("END");
+
+    return byteCount;
 }
 
 void setup() 
 {
-//	pinMode(TXPIN, OUTPUT);
-//  	digitalWrite(TXPIN, HIGH);
-
     Serial.begin(115200);
     delay(5000);
     Serial.println("Bluepill Insight DLC Tester");
-/*
-    digitalWrite(TXPIN, LOW);
-    delay(75);
-    digitalWrite(TXPIN, HIGH);
-    delay(150);
-    Serial1.begin(9600);
-    dlcInit();
-*/
 
     obd.begin(Serial1, RX_PIN, TX_PIN);
 
-    Serial.println("DLC Init Done");
-
     bool init_success =  obd.init();
-    Serial.print("init_success:");
+    Serial.print("OBD2 init_success: ");
     Serial.println(init_success);
 
     delay(500);
     dlcInit();
 
+    Serial.println("DLC Init Done");
+
 }
 
 void loop() 
 {
-    dlcCommand(0x20, 0x05, 0x08, 0x02);
+    uint8_t rxData[2];
+    int8_t res;
+    
+    res = dlcRead(0x08, 0x02, rxData);
+
+    if(res > 0)
+    {
+        Serial.print("Got Bytes: ");
+        Serial.print(res);
+        Serial.print(" Got Data: ");
+        PrintHex8(rxData[0]);
+        PrintHex8(rxData[1]);
+    }
     delay(1000);
 }
 
-/*
-void loop(){
-    Serial.println("Looping");
-
-
-
-    //init_success = true;
-    // Uncomment this line if you use the simulator to force the init to be
-    // interpreted as successful. With an actual ECU; be sure that the init is 
-    // succesful before trying to request PID's.
-
-    // Trouble code consists of a letter and then four digits, we write this
-    // human readable ascii string into the dtc_buf which we then write to the
-    // serial port.
-    uint8_t dtc_buf[5];
-
-    if (init_success){
-        uint8_t res;
-        while(1){
-            // res will hold the number of trouble codes that were received.
-            // If no diagnostic trouble codes were retrieved it will be zero.
-            // The ECU may return trouble codes which decode to P0000, this is
-            // not a real trouble code but instead used to indicate the end of
-            // the trouble code list.
-            res = obd.readTroubleCodes();
-            if (res){
-                Serial.print("Read ");
-                Serial.print(res);
-                Serial.println(" codes:");
-                for (uint8_t index = 0; index < res; index++)
-                {
-                  // retrieve the trouble code in its raw two byte value.
-                  uint16_t trouble_code = obd.getTroubleCode(index);
-
-                  // If it is equal to zero, it is not a real trouble code
-                  // but the ECU returned it, print an explanation.
-                  if (trouble_code == 0)
-                  {
-                    Serial.println("P0000 (reached end of trouble codes)");
-                  }
-                  else
-                  {
-                    // convert the DTC bytes from the buffer into readable string
-                    OBD9141::decodeDTC(trouble_code, dtc_buf);
-                    // Print the 5 readable ascii strings to the serial port.
-                    Serial.write(dtc_buf, 5);
-                    Serial.println();
-                  }
-                }
-            }
-            else
-            {
-              Serial.println("No trouble codes retrieved.");
-            }
-            Serial.println();
-
-            delay(200);
-        }
-    }
-    delay(3000);
-}
-*/
